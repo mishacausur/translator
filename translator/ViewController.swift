9dddcea9933cb138395379e745c9c21a500b688d
MishaCausur <mishacausur@mail.ru>
2021-03-03 20:46:36 +0500

    Initial Commit

---
+++ translator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  translator
+//
+//  Created by Misha Causur on 03.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
